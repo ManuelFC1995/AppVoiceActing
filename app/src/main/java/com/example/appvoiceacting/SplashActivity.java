2e501ac7a3786fd36550e780c84ae45191943c95
ManuelFC1995 <Manuelfdezco95@gmail.com>
2020-11-11 21:33:03 +0100

    Initial commit

---
+++ app/src/main/java/com/example/appvoiceacting/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.appvoiceacting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import static com.example.appvoiceacting.R.layout.activity_splash;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,ListActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
+}
